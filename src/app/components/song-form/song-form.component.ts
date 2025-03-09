12b0523e5ebf785658d5098148057ed712361a86
monguis <jccnnetwork@gmail.com>
2025-03-08 22:20:44 -0700

    style form component, added datepicker from material

--- src/app/components/song-form/song-form.component.ts
+++ src/app/components/song-form/song-form.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-song-form',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule],
   templateUrl: './song-form.component.html',
+  providers: [provideNativeDateAdapter()],
   styleUrl: './song-form.component.scss',
 })
 export class SongFormComponent {}
