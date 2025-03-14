572b78997d4ff60ab41a4dba791abefe15710127
Juan Castellanos <jccnnetwork@gmail.com>
2025-03-14 09:16:14 -0600

    Clean up songs-library tests

--- src/app/components/songs-library/songs-library.component.spec.ts
+++ src/app/components/songs-library/songs-library.component.spec.ts
@@ -55,128 +55,146 @@ describe('SongsLibraryComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call songsService.getAllSongs on ngOnInit', () => {
-    songsServiceMock.getAllSongs.and.returnValue(of([]));
+  describe('On init behavior', () => {
+    it('should call load songs when successfull on ngOnInit', () => {
+      songsServiceMock.getAllSongs.and.returnValue(of([]));
 
-    component.ngOnInit();
+      const testSong = new SongModel({
+        id: '1',
+        title: 'Test Song',
+        release_date: '01-01-2020',
+        price: 5,
+        artist: 'Test artist',
+      });
 
-    expect(songsServiceMock.getAllSongs).toHaveBeenCalled();
-    expect(component.songList$).toBe(songsServiceMock.songsList$);
-  });
+      component.songList$ = songsServiceMock.songsList$;
+      component.loadSongs();
 
-  it('should handle errors when fetching songs', () => {
-    const errorResponse = { message: 'Error fetching songs' };
-    songsServiceMock.getAllSongs.and.returnValue(throwError(errorResponse));
+      expect(songsServiceMock.getAllSongs).toHaveBeenCalled();
+      expect(component.songList$).toBe(songsServiceMock.songsList$);
+    });
 
-    component.ngOnInit();
+    it('should handle errors when fetching songs', () => {
+      const errorResponse = { message: 'Error fetching songs' };
+      songsServiceMock.getAllSongs.and.returnValue(throwError(errorResponse));
 
-    expect(notificationServiceMock.pushErrorAlert).toHaveBeenCalledWith(
-      `Songs list could not be fetched: ${errorResponse.message}`
-    );
-  });
+      component.songList$ = songsServiceMock.songsList$;
+      component.loadSongs();
 
-  it('open confirmation dialog on delete', () => {
-    songsServiceMock.deleteSong.and.returnValue(of());
-    const dialogRefMock = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
-    dialogRefMock.afterClosed.and.returnValue(of('confirm'));
-    dialogMock.open.and.returnValue(dialogRefMock);
-    component.onDelete('1');
+      expect(notificationServiceMock.pushErrorAlert).toHaveBeenCalledWith(
+        `Songs list could not be fetched: ${errorResponse.message}`
+      );
+    });
 
-    const dialogArgs = dialogMock.open.calls.mostRecent().args;
-    expect(dialogArgs[0]).toBe(ConfirmationDialogComponent);
-    expect(dialogArgs[1]).toEqual({
-      data: {
-        title: 'You are about to delete a Song.',
-        message: 'Do you want to continue?',
-      },
+    it('should open confirmation dialog on delete', () => {
+      songsServiceMock.deleteSong.and.returnValue(of());
+      const dialogRefMock = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
+      dialogRefMock.afterClosed.and.returnValue(of('confirm'));
+      dialogMock.open.and.returnValue(dialogRefMock);
+      component.onDelete('1');
+
+      const dialogArgs = dialogMock.open.calls.mostRecent().args;
+      expect(dialogArgs[0]).toBe(ConfirmationDialogComponent);
+      expect(dialogArgs[1]).toEqual({
+        data: {
+          title: 'You are about to delete a Song.',
+          message: 'Do you want to continue?',
+        },
+      });
     });
   });
 
-  it('calls handleModalDeleteConfirm on delete modal confirmation', () => {
-    const errorResponse = { message: 'Error deleting song' };
-    const handleDeleteSpy = spyOn(component, 'handleModalDeleteConfirm').and.callThrough();
+  describe('on delete behavior', () => {
+    it('calls handleModalDeleteConfirm on delete modal confirmation', () => {
+      const errorResponse = { message: 'Error deleting song' };
+      const handleDeleteSpy = spyOn(component, 'handleModalDeleteConfirm').and.callThrough();
 
-    songsServiceMock.deleteSong.and.returnValue(throwError(errorResponse));
-    const dialogRefMock = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
-    dialogRefMock.afterClosed.and.returnValue(of('confirm'));
-    dialogMock.open.and.returnValue(dialogRefMock);
-    component.onDelete('1');
+      songsServiceMock.deleteSong.and.returnValue(throwError(errorResponse));
+      const dialogRefMock = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
+      dialogRefMock.afterClosed.and.returnValue(of('confirm'));
+      dialogMock.open.and.returnValue(dialogRefMock);
+      component.onDelete('1');
 
-    dialogRefMock.afterClosed().subscribe(() => {
-      expect(handleDeleteSpy).toHaveBeenCalledWith('1');
+      dialogRefMock.afterClosed().subscribe(() => {
+        expect(handleDeleteSpy).toHaveBeenCalledWith('1');
+      });
+    });
+
+    it('should call deleteSong and removeSongFromLocalList on successful deletion', () => {
+      const testId = '1';
+      const testSong = new SongModel({
+        id: testId,
+        title: 'Test Song',
+        release_date: '01-01-2020',
+        price: 5,
+        artist: 'Test artist',
+      });
+
+      songsServiceMock.deleteSong.and.returnValue(of(testSong));
+      component.handleModalDeleteConfirm(testId);
+      expect(songsServiceMock.deleteSong).toHaveBeenCalledWith(testId);
+      expect(songsServiceMock.removeSongFromLocalList).toHaveBeenCalledWith(testId);
+    });
+
+    it('should call pushErrorAlert if deleteSong fails', () => {
+      const songId = '1';
+      const errorMessage = 'Error occurred';
+
+      songsServiceMock.deleteSong.and.returnValue(throwError({ message: errorMessage }));
+      component.handleModalDeleteConfirm(songId);
+      expect(songsServiceMock.deleteSong).toHaveBeenCalledWith(songId);
+      expect(notificationServiceMock.pushErrorAlert).toHaveBeenCalledWith(
+        `Song ID: ${songId} could not be deleted: ${errorMessage}`
+      );
     });
   });
 
-  it('should call deleteSong and removeSongFromLocalList on successful deletion', () => {
-    const testId = '1';
-    const testSong = new SongModel({
-      id: testId,
-      title: 'Test Song',
-      release_date: '01-01-2020',
-      price: 5,
-      artist: 'Test artist',
+  describe('on update behavior', () => {
+    it('navigates to song update page on onUpdate', () => {
+      const testId = '1';
+      const testSong = new SongModel({
+        id: testId,
+        title: 'Test Song',
+        release_date: '01-01-2020',
+        price: 5,
+        artist: 'Test artist',
+      });
+
+      songsServiceMock.getSong.and.returnValue(of(testSong));
+
+      component.onUpdate('1');
+
+      expect(songsServiceMock.getSong).toHaveBeenCalledWith(testId);
+      expect(songsServiceMock.setSongForEdit).toHaveBeenCalledWith(testSong);
+      expect(routerMock.navigate).toHaveBeenCalledWith(['update', testId]);
     });
 
-    songsServiceMock.deleteSong.and.returnValue(of(testSong));
-    component.handleModalDeleteConfirm(testId);
-    expect(songsServiceMock.deleteSong).toHaveBeenCalledWith(testId);
-    expect(songsServiceMock.removeSongFromLocalList).toHaveBeenCalledWith(testId);
+    it('handles error in onUpdate', () => {
+      const errorResponse = { message: 'Error fetching song' };
+      songsServiceMock.getSong.and.returnValue(throwError(errorResponse));
+      component.onUpdate('1');
+      expect(notificationServiceMock.pushErrorAlert).toHaveBeenCalledWith(
+        `Song ID: 1 could not be found in server: ${errorResponse.message}`
+      );
+    });
   });
 
-  it('should call pushErrorAlert if deleteSong fails', () => {
-    const songId = '1';
-    const errorMessage = 'Error occurred';
+  describe('on list option updates behavior', () => {
+    it('applies filters and show success message', () => {
+      const filterOptions = { sortBy: 'name', order: 'asc' } as FilterOptions<SongModel>;
+      component.onApplyFilters(filterOptions);
 
-    songsServiceMock.deleteSong.and.returnValue(throwError({ message: errorMessage }));
-    component.handleModalDeleteConfirm(songId);
-    expect(songsServiceMock.deleteSong).toHaveBeenCalledWith(songId);
-    expect(notificationServiceMock.pushErrorAlert).toHaveBeenCalledWith(
-      `Song ID: ${songId} could not be deleted: ${errorMessage}`
-    );
-  });
-
-  it('navigates to song update page on onUpdate', () => {
-    const testId = '1';
-    const testSong = new SongModel({
-      id: testId,
-      title: 'Test Song',
-      release_date: '01-01-2020',
-      price: 5,
-      artist: 'Test artist',
+      expect(component.filterOptions).toEqual(filterOptions);
+      expect(notificationServiceMock.pushSuccessAlert).toHaveBeenCalledWith(
+        'List options have been applied'
+      );
     });
 
-    songsServiceMock.getSong.and.returnValue(of(testSong));
+    it('clears filters and show warning message', () => {
+      component.onClearFilters();
 
-    component.onUpdate('1');
-
-    expect(songsServiceMock.getSong).toHaveBeenCalledWith(testId);
-    expect(songsServiceMock.setSongForEdit).toHaveBeenCalledWith(testSong);
-    expect(routerMock.navigate).toHaveBeenCalledWith(['update', testId]);
-  });
-
-  it('handles error in onUpdate', () => {
-    const errorResponse = { message: 'Error fetching song' };
-    songsServiceMock.getSong.and.returnValue(throwError(errorResponse));
-    component.onUpdate('1');
-    expect(notificationServiceMock.pushErrorAlert).toHaveBeenCalledWith(
-      `Song ID: 1 could not be found in server: ${errorResponse.message}`
-    );
-  });
-
-  it('applies filters and show success message', () => {
-    const filterOptions = { sortBy: 'name', order: 'asc' } as FilterOptions<SongModel>;
-    component.onApplyFilters(filterOptions);
-
-    expect(component.filterOptions).toEqual(filterOptions);
-    expect(notificationServiceMock.pushSuccessAlert).toHaveBeenCalledWith(
-      'List options have been applied'
-    );
-  });
-
-  it('clears filters and show warning message', () => {
-    component.onClearFilters();
-
-    expect(component.filterOptions).toEqual({});
-    expect(notificationServiceMock.pushWarningAlert).toHaveBeenCalledWith('List has been reset');
+      expect(component.filterOptions).toEqual({});
+      expect(notificationServiceMock.pushWarningAlert).toHaveBeenCalledWith('List has been reset');
+    });
   });
 });
