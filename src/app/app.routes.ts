556d67fac9de9ce0eea92fcf24b2c32f7583cb64
monguis <jccnnetwork@gmail.com>
2025-11-13 19:44:33 -0700

    implemented lazy loading to the songs form component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { SongsLibraryComponent } from './components/songs-library/songs-library.component';
-import { SongFormComponent } from './components/song-form/song-form.component';
 import { NonFoundComponent } from './components/non-found/non-found.component';
 
 export const routes: Routes = [
   { path: '', component: SongsLibraryComponent },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
-  { path: 'update/:id', component: SongFormComponent },
-  { path: 'create', component: SongFormComponent },
+  {
+    path: 'update/:id',
+    loadComponent: () =>
+      import('./components/song-form/song-form.component').then(mod => mod.SongFormComponent), // Dynamically imports and returns the component class
+  },
+  {
+    path: 'create',
+    loadComponent: () =>
+      import('./components/song-form/song-form.component').then(mod => mod.SongFormComponent), // Dynamically imports and returns the component class
+  },
   { path: '**', component: NonFoundComponent },
 ];
